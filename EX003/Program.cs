a19ebf25da434f37ca8b7817b847d3dbe8b8ea45
Nikolay <Onedaydejavu124@mail.ru>
2022-08-31 23:47:29 +0700

    Алгоритм сортировки от минимального к максимальному и от максимального к минимальному

--- EX003/Program.cs
+++ EX003/Program.cs
@@ -7,7 +7,6 @@ int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
 void PrintArray(int[] array)
 {
     int count = array.Length;
-
     for (int i = 0; i < count; i++)
     {
         Console.Write($"{array[i]} ");
@@ -20,7 +19,6 @@ void SelectionSort(int[] array)
     for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
-
         for (int j = i + 1; j < array.Length; j++)
         {
             if (array[j] < array[minPosition]) minPosition = j;
@@ -30,7 +28,23 @@ void SelectionSort(int[] array)
         array[minPosition] = temporary;
     }
 }
+void SelectionSortMax(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
 
 PrintArray(arr);
 SelectionSort(arr);
+PrintArray(arr);
+SelectionSortMax(arr);
 PrintArray(arr);
